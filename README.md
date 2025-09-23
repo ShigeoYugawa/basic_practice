52a5ea816436457c0ee97da21fcdae84e19f15ac
ShigeoYugawa <shigeo.yugawa.dev2@gmail.com>
2025-09-23 17:08:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# 基礎学習テンプレート
+
+---
+
+## 概要
+Djangoの機能学習用スターターキット。
+CusutomUserは実装済。後からユーザーにカスタムフィールドやメール認証などを追加しやすい状態になっている。
+
+---
+
+## 技術スタック
+- Python 3.12.3
+- Django 5.2.6
+
+---
+
+## インストール方法（git cloneから再現する方法）
+
+---
+
+## イチから構築する方法
+
+### 開発ディレクトリの準備
+```bash
+$ touch basic_practice
+$ cd basic_practice
+```
+
+### 仮想環境の準備
+```bash
+$ python3 -m venv .venv # 環境によってpython -m venv .venv
+$ source .venv/bin/activate
+(venv)$
+```
+
+### pip をアップデート
+```bash
+(venv)$ pip install --upgrade pip
+```
+
+### Django および必要なツールをインストール
+```bash
+(venv)$ pip install django pytest django-stubs mypy pytest-django
+```
+
+### Django プロジェクト作成
+```bash
+(venv)$ django-admin startproject basic_practice .
+```
+
+### Django の動作確認
+```bash
+(venv)$ python manage.py runserver
+```
+ブラウザで http://127.0.0.1:8000/ にアクセスして Django のウェルカムページが表示されればOK。
+
+### CustomUser を作成する
+ここにコードへのリンクを追加
+
+### CustomUserManager を作成する
+ここにコードへのリンクを追加
+
+### マイグレーションを実行する
+必ずカスタムユーザーを使ってアプリ開発できるように真っ先にマイグレーションを実行しておく
+ここではsqliteを使う
+```bash
+(venv)$ python manage.py makemigrations accounts
+```
+
+### 
+---
+
+## ライセンス
+
+このリポジトリは **学習目的のみ** で作成されています。  
+コードは学習や個人プロジェクトに使用できますが、**商用利用や再配布は禁止**です。
+
---
+++ accounts/models.py
@@ -0,0 +1,65 @@
+"""
+accounts.models モジュール
+
+このモジュールはCustomUserモデル全般の管理を担当する
+
+クラス:
+- CustomUserManager: カスタムユーザーの管理
+- CustomUser: カスタムユーザー　独自フィールドを定義するために利用する
+"""
+
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    """CustomUserモデルの管理 
+    create_user と create_superuser メソッドを提供し、ユーザー作成時の処理を統一します。
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """通常のユーザー作成"""
+        if not email:
+            raise ValueError("メールアドレスは必須です")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """管理者（スーパーユーザー）作成"""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """カスタムユーザーモデル
+
+    現在はパスワード認証を未設置
+
+    Attributes:
+        email (str): ログインで使用するメールアドレス
+        is_active (bool): アカウント有効判定　パスワード認証未設置段階では常にTrue
+        is_staff (bool): 管理画面での操作権限
+        USERNAME_FIELD (str): 認証に使用するフィールド（email）
+        REQUIRED_FIELDS (list): create_superuser で必須の追加フィールド
+    """
+
+    email = models.EmailField(verbose_name="メールアドレス", unique=True)
+    password = models.CharField(verbose_name="パスワード", max_length=128)
+    is_active = models.BooleanField(verbose_name="アカウントの有効性", default=False)
+    is_staff = models.BooleanField(verbose_name="アカウントの管理画面アクセス権限", default=False)
+
+    objects = CustomUserManager() # emailでユーザー作成するために必要。
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "ユーザー"
+        verbose_name_plural = "ユーザー一覧"
